5d6db3b9b64c921f685366e0e07cbf28c0a605d8
Lee Briggs <lee@leebriggs.co.uk>
2022-07-22 19:29:57 -0700

    finish assume role workflow

--- delivery/index.ts
+++ delivery/index.ts
@@ -1,7 +1,10 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as github from "@pulumi/github";
+import * as fs from "fs";
+import * as path from "path";
 
+// import * as mustache from mustache;
 
 // create an oidc provider that allows access from this repo
 const oidcProvider = new aws.iam.OpenIdConnectProvider("infrastructureAdmin", {
@@ -35,30 +38,78 @@ const role = new aws.iam.Role("infrastructureAdmin", {
 });
 
 // create a policy that allows us to assumeRole to any other role
-const policy = new aws.iam.Policy("infrastructureAdmin", {
-  policy: {
-    Version: "2012-10-17",
-    Statement: [
-      {
-        Action: ["sts:*"], // FIXME: can we limit this to an org maybe?
-        Effect: "Allow",
-        Resource: "*",
-      },
-    ],
-  } as aws.iam.PolicyDocument,
-}, { parent: role });
+const policy = new aws.iam.Policy(
+  "infrastructureAdmin",
+  {
+    policy: {
+      Version: "2012-10-17",
+      Statement: [
+        {
+          Action: ["sts:*"], // FIXME: can we limit this to an org maybe?
+          Effect: "Allow",
+          Resource: "*",
+        },
+      ],
+    } as aws.iam.PolicyDocument,
+  },
+  { parent: role }
+);
 
 // attach the policy
-new aws.iam.PolicyAttachment("infrastructureAdmin", {
+new aws.iam.PolicyAttachment(
+  "infrastructureAdmin",
+  {
     policyArn: policy.arn,
-    roles: [ role.id ]
-}, { parent: policy })
+    roles: [role.id],
+  },
+  { parent: policy }
+);
 
 // set the role arn in GitHub secrets
 new github.ActionsSecret("aws", {
-    repository: "lbrlabs",
-    secretName: "ROLE_ARN",
-    plaintextValue: role.arn,
-  });
+  repository: "lbrlabs",
+  secretName: "ROLE_ARN",
+  plaintextValue: role.arn,
+});
 
+/*
+ * We now need a role to assume in every AWS account in the org
+ * StackSets are the best way to this, so we're going to provision a stackset in the management account
+ */ 
+const mgmtProvider = new aws.Provider("mgmt", {
+  profile: "personal-management",
+  allowedAccountIds: ["609316800003"],
+  region: "us-west-2",
+});
 
+const stackset = new aws.cloudformation.StackSet(
+  "infrastructureAdmin",
+  {
+    templateBody: fs
+      .readFileSync(
+        path.resolve(__dirname, `template/infrastructureAdmin.yaml`) // FIXME: use mustache to interpolate the account id into here
+      )
+      .toString(),
+    permissionModel: "SERVICE_MANAGED",
+    callAs: "SELF",
+    description: "Configure an infrastructure admin role in every Org account.",
+    capabilities: ["CAPABILITY_NAMED_IAM"],
+    autoDeployment: {
+      enabled: true,
+      retainStacksOnAccountRemoval: false,
+    },
+  },
+  { provider: mgmtProvider, parent: mgmtProvider }
+);
+const infrastructureRoleStackSetInstance =
+  new aws.cloudformation.StackSetInstance(
+    "infrastructureAdmin",
+    {
+      deploymentTargets: {
+        organizationalUnitIds: ["r-p8qa"], // root ou id, all accounts
+      },
+      region: "us-west-2",
+      stackSetName: stackset.name,
+    },
+    { provider: mgmtProvider, parent: stackset }
+  );
