e7592e3153c50f28e5da77ffe2ffb8ecff1b715f
Lee Briggs <lee@leebriggs.co.uk>
2023-02-16 09:36:42 -0800

    switch VPC to python

---
+++ aws/vpc/__main__.py
@@ -0,0 +1,35 @@
+import pulumi
+import pulumi_awsx as awsx
+import pulumi_aws as aws
+
+config = pulumi.Config()
+cidr_block = config.require("cidr_block")
+number_of_availability_zones = config.require_int("number_of_availability_zones")
+enable_nat_gateway = config.require_bool("enable_nat_gateway")
+stack_name = pulumi.get_stack()
+
+if enable_nat_gateway:
+    nat_gateway_strategy = awsx.ec2.NatGatewayStrategy.ONE_PER_AZ
+else:
+    nat_gateway_strategy = awsx.ec2.NatGatewayStrategy.NONE
+
+
+vpc = awsx.ec2.Vpc(
+    f"vpc-{stack_name}",
+    cidr_block=cidr_block,
+    subnet_specs=[
+        awsx.ec2.SubnetSpecArgs(
+            type="public",
+        ),
+        awsx.ec2.SubnetSpecArgs(
+            type="private",
+        ),
+    ],
+    number_of_availability_zones=number_of_availability_zones,
+    nat_gateways=awsx.ec2.NatGatewayConfigurationArgs(strategy=nat_gateway_strategy),
+)
+
+pulumi.export("vpc_id", vpc.vpc_id)
+pulumi.export("public_subnet_ids", vpc.public_subnet_ids)
+pulumi.export("private_subnet_ids", vpc.private_subnet_ids)
+pulumi.export("cidr_block", cidr_block)
