b506e4686b885fcb835c659a499461cba4e7c8a4
Lee Briggs <lee@leebriggs.co.uk>
2023-02-16 08:53:14 -0800

    add global VPC

--- aws/vpc/index.ts
+++ aws/vpc/index.ts
@@ -2,8 +2,24 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-const vpc = new awsx.ec2.Vpc("briggs", {
-  cidrBlock: "172.16.0.0/16",
+const stack = pulumi.getStack();
+const config = new pulumi.Config();
+const cidrBlock = config.require("cidrBlock");
+const enableNatGateways = config.requireBoolean("enableNatGateways");
+const numberOfAvailabilityZones = config.requireNumber(
+  "numberOfAvailabilityZones"
+);
+
+let natGatewayStrategy: awsx.ec2.NatGatewayStrategy;
+
+if (enableNatGateways) {
+  natGatewayStrategy = awsx.ec2.NatGatewayStrategy.Single;
+} else {
+  natGatewayStrategy = awsx.ec2.NatGatewayStrategy.None;
+}
+
+const vpc = new awsx.ec2.Vpc(`${stack}-vpc`, {
+  cidrBlock: cidrBlock,
   subnetSpecs: [
     {
       type: "Public",
@@ -12,17 +28,16 @@ const vpc = new awsx.ec2.Vpc("briggs", {
       type: "Private",
     },
   ],
-  numberOfAvailabilityZones: 3,
+  numberOfAvailabilityZones: numberOfAvailabilityZones,
   natGateways: {
-    strategy: awsx.ec2.NatGatewayStrategy.Single,
+    strategy: natGatewayStrategy,
   },
   tags: {
-    Name: "brig.gs",
-    Project: "kutt",
-  }
+    Stack: stack,
+    Github: "jaxxstorm/lbrlabs",
+  },
 });
 
 export const vpcId = vpc.vpcId;
 export const publicSubnetIds = vpc.publicSubnetIds;
 export const privateSubnetIds = vpc.privateSubnetIds;
-
